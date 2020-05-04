9f4d8bec7c8444713e32a9e5d8061a079268be91
Dana McKiernan <dana.mckiernan@code42.com>
2020-05-04 16:48:33 -0500

    Putting back to raw SSH url, to test ssh known key population

--- main.go
+++ main.go
@@ -171,7 +171,7 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 
 		jobArgs := NomadJobData{
 			GitRepoName: e.Repo.GetName(),
-			GitRepoURL:  e.Repo.GetURL(),
+			GitRepoURL:  e.Repo.GetSSHURL(),
 			HeadSHA:     e.GetAfter(),
 			SSHKey:      viper.GetString("SSH_PRIV_KEY"),
 			Environment: env,
@@ -256,7 +256,7 @@ job "dir2consul-{{ .GitRepoName }}" {
         task "dir2consul" {
             artifact {
                 destination = "local/{{ .GitRepoName }}"
-                source = "git::{{ .GitRepoURL }}"
+                source = "{{ .GitRepoURL }}"
                 options {
                     sshkey = "{{ .SSHKey }}"
                 }
